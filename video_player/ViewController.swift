a82de419c27ac90d6feec5321deb889bb5ca9e13
Alberto Garcia <111655060+algarna@users.noreply.github.com>
2022-11-18 12:24:38 +0100

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Alberto Garcia on 18/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
